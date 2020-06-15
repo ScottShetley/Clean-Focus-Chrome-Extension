5a61902c3bc10a24ab4ca5c660943d1db66506d3
ScottShetley <davidscottshetley@gmail.com>
2020-06-15 11:58:13 -0700

    Dynamic Landing Page

---
+++ main.js
@@ -0,0 +1,115 @@
+// DOM elements
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
+
+// optional AM or PM
+const showAmPm = true;
+
+
+// Show Time using variables and functions
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    // Set Am or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+
+    // 12 hour format
+    hour = hour % 12 || 12;
+
+    // output time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
+
+    setTimeout(showTime, 1000);
+
+}
+
+// adds zero
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12) {
+        // Morning
+        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        greeting.textContent = 'Good Morning, ';
+    } else if (hour < 18) {
+        // Afternoon
+        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon, ';
+    } else {
+        // Evening
+        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
+    }
+}
+
+// get name
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// set name
+function setName(e) {
+    if (e.type === 'keypress') {
+        // make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+
+// get focus
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        // make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+
+
+// Runs it
+showTime();
+setBgGreet();
+getName();
+getFocus();
+
